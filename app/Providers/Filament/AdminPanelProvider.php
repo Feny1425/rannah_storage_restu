3c215eb72493a8f558e90cb1cb716d97c3c1e078
Saad5400 <sdbtwa@gmail.com>
2023-12-31 15:02:07 +0300

    Filament Spatie Init

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Althinect\FilamentSpatieRolesPermissions\FilamentSpatieRolesPermissionsPlugin;
 use App\Filament\Resources\BranchResource;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -26,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->plugin(FilamentSpatieRolesPermissionsPlugin::make())
             ->id('admin')
             ->path('')
             ->login()
