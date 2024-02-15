df083aa50f8b535729bd460ee8fcbfe029f667dc
Feny <ehab1nmy@gmail.com>
2024-02-15 22:14:34 +0300

    better seeder

--- database/seeders/MealItemSeeder.php
+++ database/seeders/MealItemSeeder.php
@@ -13,92 +13,34 @@ class MealItemSeeder extends Seeder
      */
     public function run(): void
     {
-        MealItem::createMany([
+        self::addMealItem(1, 1, 1);
+        self::addMealItem(1, 4, 5);
+        self::addMealItem(1, 6, 6);
+
+        self::addMealItem(2, 3, 7);
+        self::addMealItem(2, 4, 8);
+
+        self::addMealItem(3, 6, 4);
+        self::addMealItem(3, 3, 5);
+
+        self::addMealItem(4, 5, 6);
+
+        self::addMealItem(5, 10, 7);
+        self::addMealItem(5, 11, 8);
+        
+        self::addMealItem(6, 15, 9);
+        self::addMealItem(6, 24, 4);
+        self::addMealItem(6, 1, 2);
+    }
+
+    public function addMealItem(int $meal_id, int $item_id, int $quantity)
+    {
+        MealItem::create(
             [
-                'meal_id' => 1,
-                'item_id' => 1,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 1,
-                'item_id' => 9,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 2,
-                'item_id' => 1,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 2,
-                'item_id' => 9,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 2,
-                'item_id' => 10,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 3,
-                'item_id' => 1,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 3,
-                'item_id' => 9,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 3,
-                'item_id' => 11,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 4,
-                'item_id' => 5,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 4,
-                'item_id' => 9,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 4,
-                'item_id' => 10,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 5,
-                'item_id' => 5,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 5,
-                'item_id' => 9,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 5,
-                'item_id' => 11,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 6,
-                'item_id' => 5,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 6,
-                'item_id' => 9,
-                'quantity' => 1,
-            ],
-            [
-                'meal_id' => 6,
-                'item_id' => 12,
-                'quantity' => 1,
-            ],
-        ]);
+                'meal_id' => $meal_id,
+                'item_id' => $item_id,
+                'quantity' => $quantity,
+            ]
+        );
     }
 }
